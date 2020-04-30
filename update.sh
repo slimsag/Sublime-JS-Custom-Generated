3c4b9744280a0b0edcd4c70c07c844fb79423df7
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-04-30 03:12:13 -0700

    include embedded form

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JS\ Custom/Syntaxes/Default.sublime-syntax JavaScript.sublime-syntax
 cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JS\ Custom/Syntaxes/React.sublime-syntax React.sublime-syntax
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JS\ Custom/Syntaxes/~embed.sublime-syntax embed.sublime-syntax
